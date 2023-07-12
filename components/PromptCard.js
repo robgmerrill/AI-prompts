69c16cbc421f88838d69d4152816e959484fde4b
Rob Merrill <robgmerrill@gmail.com>
2023-07-11 17:45:29 -0700

    remove repeated attributes

--- components/PromptCard.js
+++ components/PromptCard.js
@@ -32,7 +32,7 @@ function PromptCard({post, handleTagClick, handleEdit, handleDelete}) {
                 </div>
             </div>
             <div className="copy_btn" onClick={handleCopy}>
-                <Image src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'} width={20} height={20} className="object-contain" width={12} height={12} />
+                <Image src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'} className="object-contain" width={12} height={12} />
             </div>
         </div>
         <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
